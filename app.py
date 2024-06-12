d9d15017cb923c51db23e035126179991245ce60
summereuna <summereuna@gmail.com>
2024-06-12 20:26:11 +0900

    add

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from flask import Flask, render_template
 import os
 import requests
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+# 포트 8080으로 설정함
+if __name__ == "__main__":
+    app.run(debug=True , port=5000)
